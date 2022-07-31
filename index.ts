ccefa1e5cb2814fdd3e4cc383feeb277d36eac7e
Simon Gander <simon_gander@nextlvlup.com>
2022-07-31 12:53:35 +0200

    add cors

--- index.ts
+++ index.ts
@@ -1,19 +1,25 @@
 import { SerialPort } from "serialport";
 import GPS from "gps";
+import { createServer } from "http";
 import { Server as IOServer } from "socket.io";
-import http from "http";
 
 const gps = new GPS();
 
 const gps_port = new SerialPort({ path: "/dev/ttyUSB1", baudRate: 115200 });
 const at_port = new SerialPort({ path: "/dev/ttyUSB3", baudRate: 115200 });
 
-at_port.write("AT+CGPSCOLD\r");
+gps_port.on("open", () => console.log("gps_port connected"));
+at_port.on("open", () => console.log("at_port connected"));
+
+at_port.write("AT+CGPSCOLD\r", () => console.log("gps initialized"));
+
 gps_port.on("data", (data) => gps.updatePartial(data));
 
-const server = http.createServer();
-const socket = new IOServer(server);
+const server = createServer();
+const socket = new IOServer(server, { cors: { origin: "*" } });
 
 gps.on("data", (data) => socket.emit("gps", { ...data, ...gps.state }));
 
-socket.listen(3000);
+socket.on("connected", () => console.log("new socket connection"));
+
+server.listen(3000, () => console.log("server is running"));
